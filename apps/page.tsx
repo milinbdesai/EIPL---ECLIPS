4537aaef6f5997cfb5b46315f08caf698b1416c5
milinbdesai <milinbdesai@gmail.com>
2026-07-10 22:53:01 +0530

    Delete apps directory

--- apps/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>Welcome to ECLIPS</h1>
-      <p>Excelsource Credit & Lending Intelligence Platform Suite</p>
-      <p>Setup is complete! Ready to add more features...</p>
-    </main>
-  )
-}
